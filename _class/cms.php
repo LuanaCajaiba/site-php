04032cf739d5e2cd96e245f93efce74fbfb65834
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-01 17:41:19 -0300

    Atualização

--- _class/cms.php
+++ _class/cms.php
@@ -26,7 +26,42 @@ private function criaBD() {
     return mysql_query($sql);
     }
 
+    //Essa função será responsável por conectar o site com o Banco de Dados
 
+    public function conectar() {
+        mysql_connect($this->host,$this->usuario,$this->senha)     //conecta ao servidor de banco de dados
+        or die("Não foi possível conectar. " . mysql_error());
+        mysql_select_db($this->bd)                                   //certifica-se de operar com bd correto
+        or die("Não foi possível selecionar o BD. " . mysql_error());  //die informa o erro
+        return $this->criaBD();
+        }
+
+
+    //essa função cria os formulários
+
+    public function display_admin() {
+        return <<<ADMIN_FORM
+        <form action="{$_SERVER[‘PHP_SELF’]}" method="post">
+        
+        <label for="titulo">Título:</label><br />
+        <input name="titulo" id="titulo" type="text" maxlength="150" />
+        <div class="clear"></div>
+        
+        <label for="conteudo">Conteúdo:</label><br />
+        <textarea name="conteudo" id="conteudo"></textarea>
+        <div class="clear"></div>
+        
+        <input type="submit" value="Criar Post!" />
+        </form>
+        
+        <br />
+        
+        <a href="blog.php">Voltar para Home</a>
+        ADMIN_FORM;
+        }
+
+
+        
 
 }
 ?>
