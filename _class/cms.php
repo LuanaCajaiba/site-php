2a6b58b11a2360f34e4647ac27f38364bd4e6611
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-01 17:29:44 -0300

    Atualização

--- _class/cms.php
+++ _class/cms.php
@@ -9,5 +9,24 @@ public function display_admin() {}
 public function gravar() {}
 public function conectar() {}
 private function criaBD() {}
+
+
+// Essa função irá criar as tabelas no Banco de Dados
+
+private function criaBD() {
+    $sql = <<<MySQL_QUERY
+    CREATE TABLE IF NOT EXISTS artigos (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(150),
+    conteudo TEXT,
+    data VARCHAR(100),
+    PRIMARY KEY (id)
+    )
+    MySQL_QUERY;
+    return mysql_query($sql);
+    }
+
+
+
 }
 ?>
